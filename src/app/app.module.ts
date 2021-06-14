baf3eb50782417294500e4e9d273dd6295a3d977
rickyurvina <ricky_uc12@hotmail.com>
2021-06-14 17:36:21 -0500

    fixex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -178,7 +178,7 @@ registerLocaleData(localeEs,'es');
     TranslateModule.forRoot({
       loader:{
         provide:TranslateLoader,
-        useFactory:HttpLoaderFactory,
+        useFactory:httpTranslateLoader,
         deps:[HttpClient]
       }
     })
@@ -190,6 +190,7 @@ registerLocaleData(localeEs,'es');
 })
 export class AppModule { }
 
-export function HttpLoaderFactory(http: HttpClient){
-  return new TranslateHttpLoader(http);
+// AOT compilation support
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
